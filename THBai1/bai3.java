00badc54332b4a586084cb5e936332c2f41b114e
Zianggg <nguyengiang01122005@gmail.com>
2024-10-14 13:27:26 +0700

    update

--- THBai1/bai3.java
+++ THBai1/bai3.java
@@ -7,16 +7,14 @@ class SinhVien {
     private String ngaySinh;
     private float diemGPA;
 
-    // Hàm khởi tạo không tham số
     public SinhVien() {
-        this.maSV = "B20DCCN001";  // Gán mã sinh viên mặc định
-        this.hoTen = "";            // Gán họ tên mặc định là chuỗi rỗng
-        this.lop = "";              // Gán lớp mặc định là chuỗi rỗng
-        this.ngaySinh = "";         // Gán ngày sinh mặc định là chuỗi rỗng
-        this.diemGPA = 0.0f;        // Gán điểm GPA mặc định là 0
+        this.maSV = "B20DCCN001";  
+        this.hoTen = "";           
+        this.lop = "";              
+        this.ngaySinh = "";       
+        this.diemGPA = 0.0f;       
     }
 
-    // Hàm để đọc thông tin sinh viên từ bàn phím
     public void nhapThongTin() {
         Scanner scanner = new Scanner(System.in);
         
@@ -29,19 +27,17 @@ class SinhVien {
         diemGPA = scanner.nextFloat();
     }
 
-    // Hàm để chuẩn hóa ngày sinh
     public String chuanHoaNgaySinh() {
         String[] parts = ngaySinh.split("/");
         if (parts.length == 3) {
-            String ngay = String.format("%02d", Integer.parseInt(parts[0])); // Đảm bảo là 2 chữ số
-            String thang = String.format("%02d", Integer.parseInt(parts[1])); // Đảm bảo là 2 chữ số
-            String nam = parts[2]; // Giữ nguyên năm
+            String ngay = String.format("%02d", Integer.parseInt(parts[0])); 
+            String thang = String.format("%02d", Integer.parseInt(parts[1])); 
+            String nam = parts[2]; 
             return ngay + "/" + thang + "/" + nam;
         }
-        return ngaySinh; // Trả lại nguyên nếu không đúng định dạng
+        return ngaySinh; 
     }
 
-    // Hàm để in thông tin sinh viên
     public void inThongTin() {
         System.out.println(maSV + " " + hoTen + " " + lop + " " + chuanHoaNgaySinh() + " " + String.format("%.2f", diemGPA));
     }
@@ -49,8 +45,8 @@ class SinhVien {
 
 public class bai3 {
     public static void main(String[] args) {
-        SinhVien sinhVien = new SinhVien(); // Tạo đối tượng sinh viên
-        sinhVien.nhapThongTin();            // Nhập thông tin
-        sinhVien.inThongTin();               // In thông tin
+        SinhVien sinhVien = new SinhVien(); 
+        sinhVien.nhapThongTin();            
+        sinhVien.inThongTin();              
     }
 }
--- THBai1/bai4.java
+++ THBai1/bai4.java
@@ -9,18 +9,16 @@ class NhanVien {
     private String maSoThue;
     private String ngayKyHopDong;
 
-    // Hàm khởi tạo không tham số
     public NhanVien() {
-        this.maNV = "00001";  // Gán mã nhân viên mặc định
-        this.hoTen = "";       // Gán họ tên mặc định là chuỗi rỗng
-        this.gioiTinh = "";    // Gán giới tính mặc định là chuỗi rỗng
-        this.ngaySinh = "";    // Gán ngày sinh mặc định là chuỗi rỗng
-        this.diaChi = "";      // Gán địa chỉ mặc định là chuỗi rỗng
-        this.maSoThue = "";    // Gán mã số thuế mặc định là chuỗi rỗng
-        this.ngayKyHopDong = ""; // Gán ngày ký hợp đồng mặc định là chuỗi rỗng
+        this.maNV = "00001";  
+        this.hoTen = "";       
+        this.gioiTinh = "";    
+        this.ngaySinh = "";    
+        this.diaChi = "";      
+        this.maSoThue = "";    
+        this.ngayKyHopDong = ""; 
     }
 
-    // Hàm để đọc thông tin nhân viên từ bàn phím
     public void nhapThongTin() {
         Scanner scanner = new Scanner(System.in);
         
@@ -42,7 +40,6 @@ class NhanVien {
         ngayKyHopDong = scanner.nextLine();
     }
 
-    // Hàm để in thông tin nhân viên
     public void inThongTin() {
         System.out.println(maNV + " " + hoTen + " " + gioiTinh + " " + ngaySinh + " " + diaChi + " " + maSoThue + " " + ngayKyHopDong);
     }
@@ -50,8 +47,8 @@ class NhanVien {
 
 public class bai4 {
     public static void main(String[] args) {
-        NhanVien nhanVien = new NhanVien(); // Tạo đối tượng nhân viên
-        nhanVien.nhapThongTin();            // Nhập thông tin
-        nhanVien.inThongTin();               // In thông tin
+        NhanVien nhanVien = new NhanVien();
+        nhanVien.nhapThongTin();            
+        nhanVien.inThongTin();         
     }
 }
